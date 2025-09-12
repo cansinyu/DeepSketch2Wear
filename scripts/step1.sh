90a0485d880cc812be2fa2a02860e1714a47f58e
Cansinyu <166212974+cansinyu@users.noreply.github.com>
2025-09-12 17:03:00 +0800

    Update and rename generate.sh to step1.sh

--- scripts/step1.sh
+++ scripts/step1.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 python generate_udf.py \
 --generate_method generate_based_on_sketch_batch \
---model_path results/udf_power/epoch=999.ckpt \
+--model_path results/udf/epoch=999.ckpt \
 --sketch_file /public/home/chenxinyu2023/3D/data/step2_data/test.txt \
 --output_path outputs \
 --num_generate 3 \
