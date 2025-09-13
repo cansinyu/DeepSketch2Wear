7ef0042348373211fd800ee3e68f18beb078065d
Cansinyu <166212974+cansinyu@users.noreply.github.com>
2025-09-13 22:46:43 +0800

    Delete scripts directory

--- scripts/step1.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-python generate_udf.py \
---generate_method generate_based_on_sketch_batch \
---model_path results/udf/epoch=999.ckpt \
---sketch_file /public/home/chenxinyu2023/3D/data/step2_data/test.txt \
---output_path outputs \
---num_generate 3 \
---steps 20 
-
--- scripts/step2.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-python udf2mesh.py \
---root_dir "output/udf_power/4-13"
